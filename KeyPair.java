fcc308b8cd37613d986104a4e9173d5690b65fd8
silas magu <107644537+silaslovescode@users.noreply.github.com>
2022-10-29 15:39:55 +0300

    Add files via upload

---
+++ KeyPair.java
@@ -0,0 +1,15 @@
+package BABY-BLOCKCHAIN;
+
+public class KeyPair {
+
+  public privateKey privateKey;
+
+  public publicKey publicKey;
+
+  public void public static KeyPair genKeyPair() {
+  }
+
+  public void public void printKeyPair() {
+  }
+
+}
---
+++ Signature.java
@@ -0,0 +1,22 @@
+package BABY-BLOCKCHAIN;
+
+import Integer;
+import java.util.Vector;
+
+public class Signature extends KeyPair, KeyPair {
+
+  public Integer newAttr;
+
+    public Vector  mySignature;
+    public Vector  mySignature;
+
+  public void static [] signDatabyte() {
+  }
+
+  public void public static boolean verifySignature() {
+  }
+
+  public void public static void printSignature() {
+  }
+
+}
